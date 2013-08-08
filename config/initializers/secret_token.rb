c9ff26f701db0ad3650c9e8319c6e6ced3ff6332
robert lu <dao888@gmail.com>
2013-08-07 20:00:43 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raboot::Application.config.secret_token = '33fb67a6672bead63af66dd08164f46654e4c3ed1e8f2a4317c3926130725e56d6ab3205edaae4462b9bb30d34ab9dd709eaa1c897c9ccb2f6323c4edb209326'
